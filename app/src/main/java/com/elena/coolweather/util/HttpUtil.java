5653d876fa9bfe5a2b99f3fc56ac0cd8f3d47bec
Elena <2491411913@qq.com>
2017-12-01 16:32:36 +0800

    完成遍历省市县三级列表的功能。

---
+++ app/src/main/java/com/elena/coolweather/util/HttpUtil.java
@@ -0,0 +1,16 @@
+package com.elena.coolweather.util;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+
+/**
+ * Created by Elena on 2017/11/30.
+ */
+
+public class HttpUtil {
+    public static void sendOkHttpRequest(String address,okhttp3.Callback callback){
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder().url(address).build();
+        client.newCall(request).enqueue(callback);
+    }
+}
